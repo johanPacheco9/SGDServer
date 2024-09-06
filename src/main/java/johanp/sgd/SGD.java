ea4e76b42c70fc59e1948436ef08a978d233a0d1
johanPacheco9 <johanpach9@gmail.com>
2024-09-05 23:38:40 -0500

    'main'

--- src/main/java/johanp/sgd/SGD.java
+++ src/main/java/johanp/sgd/SGD.java
@@ -4,13 +4,44 @@
 
 package johanp.sgd;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import johanp.Domain.Services.FileF.FIleService;
+import johanp.Domain.Services.FileF.IFileService;
+
 /**
  *
  * @author johan
  */
 public class SGD {
 
+      private static final int PUERTO = 8083; // Puerto para UserService
+  
+    
+    private static final String SERVICIO_NOMBRE = "fileService";
+ 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      try {
+           
+            Registry registryFileService = LocateRegistry.createRegistry(PUERTO);
+            
+   
+            IFileService fileService = new FIleService();
+            
+            
+            registryFileService.rebind(SERVICIO_NOMBRE, fileService);
+           
+            
+            
+
+          
+            
+            
+            System.out.println("FileService corriendo en el puerto:" + PUERTO);
+            
+        } catch (RemoteException e) {
+            System.out.println("Error de RMI: " + e.getMessage());
+        }
     }
 }
