4074b90c1622c6fac45f38e34745f2efd14b5d06
johanPacheco9 <johanpach9@gmail.com>
2024-09-06 13:28:55 -0500

    'okey'

--- src/main/java/johanp/sgd/SGD.java
+++ src/main/java/johanp/sgd/SGD.java
@@ -1,3 +1,6 @@
+package johanp.sgd;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -8,8 +11,18 @@ public class SGD {
 
     private static final int PUERTO = 8083; // Puerto para UserService
     private static final String SERVICIO_NOMBRE = "fileService";
+    private static final String FILE_DIRECTORY = "/home/cliente/share/";
 
     public static void main(String[] args) {
+        java.io.File fileToSave = new java.io.File(FILE_DIRECTORY + "testfile.txt");
+        try (FileOutputStream fileOutput = new FileOutputStream(fileToSave)) {
+            String content = "This is a test.";
+            fileOutput.write(content.getBytes());
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try {
             // Configura la IP del servidor RMI
             System.setProperty("java.rmi.server.hostname", "192.168.1.15"); // Cambia la IP por la correcta
