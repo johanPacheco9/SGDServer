cb66dfc9429e85f982791e38bab81e63d75e4375
johanPacheco9 <johanpach9@gmail.com>
2024-09-06 00:40:27 -0500

    'main1'

--- src/main/java/johanp/sgd/SGD.java
+++ src/main/java/johanp/sgd/SGD.java
@@ -1,44 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package johanp.sgd;
-
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import johanp.Domain.Services.FileF.FIleService;
 import johanp.Domain.Services.FileF.IFileService;
 
-/**
- *
- * @author johan
- */
 public class SGD {
 
-      private static final int PUERTO = 8083; // Puerto para UserService
-  
-    
+    private static final int PUERTO = 8083; // Puerto para UserService
     private static final String SERVICIO_NOMBRE = "fileService";
- 
+
     public static void main(String[] args) {
-      try {
-           
+        try {
+            // Configura la IP del servidor RMI
+            System.setProperty("java.rmi.server.hostname", "192.168.1.15"); // Cambia la IP por la correcta
+            
             Registry registryFileService = LocateRegistry.createRegistry(PUERTO);
             
-   
             IFileService fileService = new FIleService();
             
-            
             registryFileService.rebind(SERVICIO_NOMBRE, fileService);
-           
             
-            
-
-          
-            
-            
-            System.out.println("FileService corriendo en el puerto:" + PUERTO);
+            System.out.println("FileService corriendo en el puerto: " + PUERTO);
             
         } catch (RemoteException e) {
             System.out.println("Error de RMI: " + e.getMessage());
